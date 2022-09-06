4c9471fe3bd48e694df7db90d60d83d86fff5c8b
sbx_user1051 <sbx_user1051@169.254.22.149>
2022-09-06 22:25:04 +0000

    Tue Sep 06 22:25:04 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -8,15 +8,39 @@ public class CheckoutSolution {
 	private static Map<String, Integer> skus = new HashMap<String, Integer>();	
 	
 	static {
-		skus.
+		
+	//	
+//		+------+-------+----------------+
+//		| Item | Price | Special offers |
+//		+------+-------+----------------+
+//		| A    | 50    | 3A for 130     |
+//		| B    | 30    | 2B for 45      |
+//		| C    | 20    |                |
+//		| D    | 15    |                |
+//		+------+-------+----------------+
+		
+		skus.put("A", 50);
+		skus.put("B", 30);
+		skus.put("A", 20);
+		skus.put("A", 15);		
 	}
 	
+
+
+	
     public Integer checkout(String skus) {
+    	
+    	String items [] =  skus.split(",");
+    	
+    	for(int i = 0 ; i < items.length; i ++) {
+    		
+    	}
     	    	
     	
         return 0;
     }
 }
+
 
 
 
