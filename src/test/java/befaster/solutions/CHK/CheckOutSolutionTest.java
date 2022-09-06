4f69cc5497ec83f5231416c713798cee4fcff6e3
sbx_user1051 <sbx_user1051@169.254.22.149>
2022-09-06 22:13:04 +0000

    Tue Sep 06 22:13:04 UTC 2022

---
+++ src/test/java/befaster/solutions/CHK/CheckOutSolutionTest.java
@@ -0,0 +1,17 @@
+package befaster.solutions.CHK;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CheckOutSolutionTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testTotalItensNoDiscount() {
+		
+	}
+
+}
