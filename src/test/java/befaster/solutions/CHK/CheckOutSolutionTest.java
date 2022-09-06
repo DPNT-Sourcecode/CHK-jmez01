f0be1098b82e4e46571b6e681f6c6b24a189c0eb
sbx_user1051 <sbx_user1051@169.254.22.149>
2022-09-06 22:16:04 +0000

    Tue Sep 06 22:16:04 UTC 2022

--- src/test/java/befaster/solutions/CHK/CheckOutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckOutSolutionTest.java
@@ -1,17 +1,28 @@
 package befaster.solutions.CHK;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class CheckOutSolutionTest {
+	
+	private CheckoutSolution checkOutSolution;
 
 	@Before
 	public void setUp() throws Exception {
+		
+		checkOutSolution = new CheckoutSolution();
 	}
 
 	@Test
 	public void testTotalItensNoDiscount() {
 		
+		Integer actual = checkOutSolution.checkout("");
+		Integer expected = 2;
+		assertEquals(actual, expected);		
+		
 	}
 
 }
+
