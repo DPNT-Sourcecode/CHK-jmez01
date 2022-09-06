a2d3f7c5864c9b297dc8180d014e06088f0e724d
sbx_user1051 <sbx_user1051@169.254.22.149>
2022-09-06 23:48:57 +0000

    Tue Sep 06 23:48:57 UTC 2022

--- src/test/java/befaster/solutions/CHK/CheckOutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckOutSolutionTest.java
@@ -78,10 +78,54 @@ public class CheckOutSolutionTest {
 		
 	}
 	
+
+//	id = CHK_R1_012, req = checkout("A"), resp = 50
+//	id = CHK_R1_013, req = checkout("AA"), resp = -1
+//	id = CHK_R1_014, req = checkout("AAA"), resp = -1
+//	id = CHK_R1_015, req = checkout("AAAA"), resp = -1
+//	id = CHK_R1_016, req = checkout("AAAAA"), resp = -1
+//	id = CHK_R1_017, req = checkout("AAAAAA"), resp = -1
+//	id = CHK_R1_018, req = checkout("B"), resp = 30
+//	id = CHK_R1_019, req = checkout("BB"), resp = -1
+//	id = CHK_R1_020, req = checkout("BBB"), resp = -1
+//	id = CHK_R1_021, req = checkout("BBBB"), resp = -1
+//	id = CHK_R1_022, req = checkout("ABCDABCD"), resp = -1
+//	id = CHK_R1_023, req = checkout("BABDDCAC"), resp = -1
+//	id = CHK_R1_024, req = checkout("AAABB"), resp = -1
+//	id = CHK_R1_001, req = checkout("ABCDCBAABCABBAAA"), resp = -1
+//
+	
 	@Test
 	public void testTotalItens_Ilegal_ABCa() {
 		
 		Integer actual = checkOutSolution.checkout("ABCa");
+		Integer expected = -1;
+		assertEquals(expected, actual);		
+		
+	}	
+	
+	@Test
+	public void testTotalItens_Ilegal_AxA() {
+		
+		Integer actual = checkOutSolution.checkout("AxA");
+		Integer expected = -1;
+		assertEquals(expected, actual);		
+		
+	}	
+	
+	@Test
+	public void testTotalItens_Ilegal_ABCD() {
+		
+		Integer actual = checkOutSolution.checkout("ABCD");
+		Integer expected = -1;
+		assertEquals(expected, actual);		
+		
+	}	
+	
+	@Test
+	public void testTotalItens_Ilegal_AA() {
+		
+		Integer actual = checkOutSolution.checkout("AA");
 		Integer expected = -1;
 		assertEquals(expected, actual);		
 		
@@ -154,6 +198,7 @@ public class CheckOutSolutionTest {
 
 
 }
+
 
 
 
