72f72c25cdd888fb431e919d1de06ac8d73f1fa3
sbx_user1051 <sbx_user1051@169.254.22.149>
2022-09-06 22:22:04 +0000

    Tue Sep 06 22:22:04 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,22 @@
 package befaster.solutions.CHK;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
+	
+	private static Map<String, Integer> skus = new HashMap<String, Integer>();	
+	
+	static {
+		skus.
+	}
+	
     public Integer checkout(String skus) {
+    	    	
+    	
         return 0;
     }
 }
+
 
 
